5428a7a36f774b8427667c0518fb97e18c37a4a2
keppuraja3 <132921416+keppuraja3@users.noreply.github.com>
2023-08-19 17:08:38 +0530

    All files uploaded

---
+++ Script.js
@@ -0,0 +1,10 @@
+function welcome_page(){
+    location.href="welcome.html";
+}
+
+function gift_click(){
+    location.href="gift_open.html";
+}
+function gallery(){
+    location.href="gallery.html";
+}
